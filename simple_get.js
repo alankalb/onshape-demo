253eca0bd03ade784a3ff7cd1a67e9c8ac9647b4
Alan Kalbfleisch <alan.kalbfleisch@shopify.com>
2019-11-24 20:34:09 -0500

    get and save features

--- simple_get.js
+++ simple_get.js
@@ -1,5 +1,6 @@
 var client = require("./onshape/apikey/lib/app");
 
+const fs = require("fs");
 require("dotenv").config();
 
 var apiKeyDetails = {
@@ -8,6 +9,21 @@ var apiKeyDetails = {
   secretKey: process.env.ONSHAPE_SECRET_KEY
 };
 
-client(apiKeyDetails).getDocuments({}, function(data) {
-  console.log(data);
+var documentId = "547e3732e044297245400441";
+var workspaceId = "a4172eaa2592099428ac79c0";
+var elementId = "397254c5d1fc59b540638ee2";
+
+client(apiKeyDetails).getFeatures(documentId, workspaceId, elementId, function(
+  data
+) {
+  writeFile(data);
 });
+
+async function writeFile(data) {
+  fs.writeFile("onshape_features.json", data, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("file was saved");
+  });
+}
